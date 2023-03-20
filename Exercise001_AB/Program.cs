0893f7a89af8c9bcc3ac67059889bf1ac24dda6c
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-03-20 14:13:40 +0300

    Done Exercise001

---
+++ Exercise001_AB/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+int Text(string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int n1  = Text("Enter a base: "); // Вводим основание
+int n2  = Text("Enter a extent: "); // Вводим степень
+
+int Construction(int basement, int extent)
+{
+    int result = 1;
+    for (int i = 1; i <= extent; i++)
+    {
+        result = result * basement;
+    }
+    return result;
+}
+
+Console.Write($"Your value is: {Construction(n1, n2)}");
